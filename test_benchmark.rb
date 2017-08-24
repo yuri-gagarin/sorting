8b9381741642ceda9f7624625a3e86b865136d50
Yuriy Ivanov <ymivanov@icloud.com>
2017-08-24 13:32:00 -0400

    larger array in test case

--- test_benchmark.rb
+++ test_benchmark.rb
@@ -7,7 +7,7 @@ require "./insertion_sort.rb"
 
 
 test_array = [14, 5, 16, 12, 1, 19, 23, 11, -2, -1, 
- 			   0, 22, 51, 17, 6, -31, 2, -100, 54]
+ 			   0, 22, 51, 17, 6, -31, 2, -100, 54, 343, 12, 434, -65, -134, 412, 58, 12, 74, 39, 82, -98]
 
 
 puts Benchmark.measure { HeapSort.sort(test_array) }
